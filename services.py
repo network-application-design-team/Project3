8c7743d436ac418c309001617a3f2e52040046f4
Honaker <chris98@vt.edu>
2019-04-20 10:19:46 -0400

    flask

--- services.py
+++ services.py
@@ -1,1 +1,31 @@
+from six.moves import input  
+from zeroconf import ServiceBrowser, Zeroconf
+from flask import Flask, render_template
+import datetime
+app = Flask(__name__)
 
+class MyListener(object):  
+    def remove_service(self, zeroconf, type, name):
+        print("Service %s removed" % (name,))
+
+    def add_service(self, zeroconf, type, name):
+        info = zeroconf.get_service_info(type, name)
+        # print name, info.get_name(), info.server,
+#        print name, info
+
+
+zeroconf = Zeroconf()  
+listener = MyListener()  
+browser = ServiceBrowser(zeroconf, "_http._tcp.local.", listener)  
+@app.route("/")
+def hello():
+   now = datetime.datetime.now()
+   timeString = now.strftime("%Y-%m-%d %H:%M")
+   templateData = {
+      'title' : 'HELLO!',
+      'time': timeString
+      }
+   return render_template('main.html', **templateData)
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=80, debug=True)
