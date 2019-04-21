c5505facff774c84f068e830aa31e5003fa69f2e
ethanb98 <ethanb98@vt.edu>
2019-04-21 17:21:31 -0400

    Added pwm abilities

--- led.py
+++ led.py
@@ -25,13 +25,24 @@ listener = MyListener()
 browser = ServiceBrowser(zeroconf, "_http._tcp.local.", listener)
 ipAddr = fetch_ip()
 
-
+"""Set led ports"""
 redPin = 11
 greenPin = 13
 bluePin = 15
 
+"""PWM functions"""
+pwmRed = GPIO.PWM(11, 100)
+pwmGreen = GPIO.PWM(13, 100)
+pwmBlue = GPIO.PWM(15, 100)
+dc = 100
+#dcRed = 100
+#dcGreen = 100
+#dcBlue = 100
+
+"""turn off warnings"""
 GPIO.setwarnings(False)
 
+"""LED Functions"""
 def blink(pin):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(pin, GPIO.OUT)
@@ -42,56 +53,88 @@ def turnOff(pin):
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.LOW)
 
+"""PWM lightups"""
+def bright(pwm, newdc):
+    pwm.start(100)
+    pwm.ChangeDutyCycle(newdc)
+
+def dark(pwm):
+    pwm.ChangeDutyCycle(0)
+    pwm.stop()
+
+"""LED and PWM lightups"""
 def redOn():
     blink(redPin)
+    bright(pwmRed, dc)
 
 def greenOn():
     blink(greenPin)
+    bright(pwmGreen, dc)
 
 def blueOn():
     blink(bluePin)
+    bright(pwmBlue, dc)
 
 def yellowOn():
     blink(redPin)
+    bright(pwmRed, dc)
     blink(greenPin)
+    bright(pwmGreen, dc)
 
 def cyanOn():
     blink(greenPin)
+    bright(pwmGreen, dc)
     blink(bluePin)
+    bright(pwmBlue, dc)
 
 def magentaOn():
     blink(redPin)
+    bright(pwmRed, dc)
     blink(bluePin)
+    bright(pwmBlue, dc)
 
 def whiteOn():
     blink(redPin)
+    bright(pwmRed, dc)
     blink(greenPin)
+    bright(pwmGreen, dc)
     blink(bluePin)
+    bright(pwmBlue, dc)
 
 def redOff():
     turnOff(redPin)
+    dark(pwmRed)
 
 def greenOff():
     turnOff(greenPin)
+    dark(pwmGreen)
 
 def blueOff():
     turnOff(bluePin)
+    dark(pwmBlue)
 
 def yellowOff():
     turnOff(redPin)
+    dark(pwmRed)
     turnOff(greenPin)
+    dark(pwmGreen)
 
 def cyanOff():
     turnOff(greenPin)
+    dark(pwmGreen)
     turnOff(bluePin)
+    dark(pwmBlue)
 
 def magentaOff():
     turnOff(redPin)
+    dark(pwmRed)
     turnOff(bluePin)
+    dark(pwmBlue)
 
 def whiteOff():
     turnOff(redPin)
+    dark(pwmRed)
     turnOff(greenPin)
+    dark(pwmGreen)
     turnOff(bluePin)
-
-
+    dark(pwmBlue)
