0fed141696a4757f21121c7a70233f329c797342
ethanb98 <ethanb98@vt.edu>
2019-04-21 14:26:13 -0400

    Added basic light commands, no pwm yet

--- led.py
+++ led.py
@@ -1,5 +1,9 @@
 from zeroconf import ServiceBrowser, Zeroconf
 
+"""imports for gpio led usage"""
+import sys, time
+import RPi.GPIO as GPIO
+
 def fetch_ip():
       return((([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close())\
         for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0])
@@ -20,4 +24,74 @@ zeroconf = Zeroconf()
 listener = MyListener()
 browser = ServiceBrowser(zeroconf, "_http._tcp.local.", listener)
 ipAddr = fetch_ip()
+
+
+redPin = 11
+greenPin = 13
+bluePin = 15
+
+GPIO.setwarnings(False)
+
+def blink(pin):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
+
+def turnOff(pin):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
+
+def redOn():
+    blink(redPin)
+
+def greenOn():
+    blink(greenPin)
+
+def blueOn():
+    blink(bluePin)
+
+def yellowOn():
+    blink(redPin)
+    blink(greenPin)
+
+def cyanOn():
+    blink(greenPin)
+    blink(bluePin)
+
+def magentaOn():
+    blink(redPin)
+    blink(bluePin)
+
+def whiteOn():
+    blink(redPin)
+    blink(greenPin)
+    blink(bluePin)
+
+def redOff():
+    turnOff(redPin)
+
+def greenOff():
+    turnOff(greenPin)
+
+def blueOff():
+    turnOff(bluePin)
+
+def yellowOff():
+    turnOff(redPin)
+    turnOff(greenPin)
+
+def cyanOff():
+    turnOff(greenPin)
+    turnOff(bluePin)
+
+def magentaOff():
+    turnOff(redPin)
+    turnOff(bluePin)
+
+def whiteOff():
+    turnOff(redPin)
+    turnOff(greenPin)
+    turnOff(bluePin)
+
 
